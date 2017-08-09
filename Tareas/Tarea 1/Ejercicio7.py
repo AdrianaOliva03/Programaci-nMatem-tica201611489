2dd16b2db70e1a28e87fa90ee652d2c8e94a0937
luismack1997 <30278823+luismack1997@users.noreply.github.com>
2017-08-08 20:46:42 -0600

    Agregando la primera tarea

---
+++ Tareas/Tarea 1/Ejercicio7.py
@@ -0,0 +1,8 @@
+n = 111000
+criba = [True] * n
+
+for i in range(2, int(n ** .5) + 1):
+    for j in range(i * i, n + 1, i):
+        criba[j - 1] = False
+primos = [p for p in range(2, len(criba) + 1) if criba[p - 1]]
+print primos[10000]
